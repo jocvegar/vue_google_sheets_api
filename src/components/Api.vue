f56cbf4d43d9cb6e895fe41b8f87720f51034acb
Jose <jocvegar@gmail.com>
2020-04-06 10:05:22 -0600

    Adds pie chart

--- src/components/Api.vue
+++ src/components/Api.vue
@@ -1,36 +1,50 @@
 <template>
-    <div id="app">
+    <div id="container">
         <section v-if="errored">
             <p>No lo pude load! Try again later</p>
         </section>
         <section v-else>
             <div v-if="loading">Loading...</div>
-            <div v-else >
-                 <div class="chart-container">
-                     <line-chart  
-                    :chartdata_labels="chartdata_labels"
-                    :chartdata_data="chartdata_data" />
-                 </div>
+            <div v-else>
+                <div class="chart-container">
+                    <line-chart  
+                        :chartdata_labels="chartdata_labels"
+                        :chartdata_data="chartdata_data" />
+                    <br>
+                    <br>
+                    <pie-chart 
+                        :chartdata_pie_data_healed="chartdata_pie_data_healed"
+                        :chartdata_pie_data_deceased="chartdata_pie_data_deceased"
+                        :chartdata_pie_data_better="chartdata_pie_data_better" />    
+                    <br>
+                    <br> 
+                    <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/uqlpfWJ">
+                        <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee">
+                    </a>
+                </div>
             </div>
-        </section>    
+        </section>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 import LineChart from './LineChart.vue'
+import PieChart from './PieChart.vue'
 import moment from 'moment'
 moment.locale('es');
 
 export default {
-    components: { LineChart },
+    components: { LineChart, PieChart },
     data() {
         return {
             chartdata_labels: null,
             chartdata_data: null,
+            chartdata_pie_data_healed: null,
+            chartdata_pie_data_deceased: null,
+            chartdata_pie_data_better: null,
             loading: true,
             errored: false,
-            height: 300,
         }
     },
     async mounted() {
@@ -39,6 +53,9 @@ export default {
             .then(response => {
               this.chartdata_labels = response.data.cases.map(x => moment(x.date).format('DD [de] MMM'))     
               this.chartdata_data = response.data.cases.map(x => x.cases)     
+              this.chartdata_pie_data_healed = response.data.cases.map(x => x.healed).filter(function(e){return e})
+              this.chartdata_pie_data_deceased = response.data.cases.map(x => x.deceased).filter(function(e){return e})
+              this.chartdata_pie_data_better = response.data.cases.map(x => x.better).filter(function(e){return e})
             })
             .catch(error => {
                 console.log(error)
@@ -51,7 +68,6 @@ export default {
 
 <style>
 .chart-container {
-    position: relative;
     margin: auto;
     height: 50vh;
     width: 90vw;
---
+++ src/components/PieChart.vue
@@ -0,0 +1,63 @@
+<script>
+import { Pie } from 'vue-chartjs'
+
+export default {
+    extends: Pie,
+    props: {
+        chartdata_pie_data_healed: {
+            type: Array,
+            default: null
+        },
+        chartdata_pie_data_deceased: {
+            type: Array,
+            default: null
+        },
+        chartdata_pie_data_better: {
+            type: Array,
+            default: null
+        }
+    },
+    data() {
+        return {
+            chartdata: {
+                labels: [
+					'curados',
+					'fallecidos',
+					'por curase',
+                ],
+                datasets: [
+                    {
+                        backgroundColor: [
+                            '#42b883',
+                            '#42b840',
+                            '#aab883',
+                        ],
+                        data: [
+                            this.chartdata_pie_data_healed[0],
+                            this.chartdata_pie_data_deceased[0],
+                            this.chartdata_pie_data_better[0],
+                        ],
+                    },
+                ],
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: 'top'
+                },
+				title: {
+					display: true,
+					text: 'Eventos'
+                },
+                animation: {
+                    animateScale: true
+                }
+			}
+        }
+    },
+    mounted() {
+        this.renderChart(this.chartdata, this.options)
+    }
+}
+</script>
